66927d042048adc480a472e4b74b004c55f79c7e
Jacky <87586598+jcheng1022@users.noreply.github.com>
2024-08-19 18:01:32 -0400

    hero cta

--- components/landing/Hero.js
+++ components/landing/Hero.js
@@ -5,17 +5,28 @@ import Image from "next/image";
 import MasterLogo from '/public/master.svg'
 import {useRouter} from "next/navigation";
 import {useCurrentUser} from "@/hooks/user.hooks";
+import {useAuthContext} from "@/context/AuthContext";
 
 const Hero = () => {
     const {data: user} = useCurrentUser()
     const router = useRouter()
+    const { handleSignIn } = useAuthContext()
+
+
+    const handleUserCta = () => {
+        if (!!user) {
+            router.push(`/user/${user?.firebaseUuid}`)
+        } else {
+            handleSignIn()
+        }
+    }
     return (
         <div className={'bg-gradient-to-tr from-blue-100 to-sky-200 p-4 flex items-center justify-around'}>
             <div className={'py-24'}>
 <div className={'text-5xl font-bold text-slate-900'}>Turn your day into a game</div>
                 <p className={'text-lg text-blue-700'}>Manage your tasks and earn rewards</p>
                 <div
-                    onClick={() => router.push(`/user/${user?.firebaseUuid}`)}
+                    onClick={handleUserCta}
                     className={'bg-gradient-to-bl border-4 mt-8 from-sky-200 to-sky-300 hover:bg-gradient-to-tr border-2 cursor-pointer mt-8 from-sky-200 to-sky-300 w-64 rounded-2xl font-bold  h-16 flex items-center justify-center'}> Start Game</div>
             </div>
             <div className={'bg-gradient-to-r from-cyan-200 to-blue-600 p-4 rounded-2xl'} style={{width:200, height:200}}>
